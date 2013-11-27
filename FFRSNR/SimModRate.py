f469c75f8eb356ebe637b7f424f478a8ab714ab1
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2013-11-27 15:32:31 -0500

    Adding toy data simulations along with real EEG

--- FFRSNR/SimModRate.py
+++ FFRSNR/SimModRate.py
@@ -13,33 +13,34 @@ import pylab as pl
 
 
 # Load data recorded with a 100 Hz transposed tone
-datdir = '/home/hari/Documents/PythonCodes/research/FFRSNRstuff/'
+# datdir = '/home/hari/Documents/PythonCodes/research/FFRSNRstuff/'
+datdir = '/home/hari/Documents/MNEforBiosemi/'
 x = io.loadmat(datdir + 'x.mat')['x'][30,:,:]
 
 fs = 4096.0
 ntime = x.shape[1]
 fsig = 211.0 #Picking some frequency nowhere neaR 100 Hz
-Strue = 0.1e-6 #Typical FFR size
+Strue = 0.1e-7 #Typical FFR size
 t = np.arange(0.0,ntime)/fs
 sig = Strue*np.sin(2*np.pi*fsig*t)
 
-noisefac_list = np.arange(0.2,10.0,0.2)
-SIlist = np.zeros(noisefac_list.shape)
-Srawlist = np.zeros(noisefac_list.shape)
+noisefac_list = np.arange(0.2,5.0,0.2)
+Slist = np.zeros(noisefac_list.shape)
 pspeclist = np.zeros(noisefac_list.shape)
 for k, noisefac in enumerate(noisefac_list):
     print 'Testing Noise Level #',k
     y = sig + x*noisefac
     # Comuting spectrum and phase-locking
-    params = dict(Fs = fs, tapers = [1, 1], pad = 1,
-              fpass = [5,400], Npairs = 5000, itc = 1)
-    [Sraw,f] = spectral.mtspecraw(y, params)
-    [pspec,f] = spectral.mtpspec(y, params)
-    Srawlist[k] = max(Sraw[abs(f - fsig) < 1])
+    params = dict(Fs = fs, tapers = [2, 3], pad = 1,
+              fpass = [5,400], Npairs = 1000, itc = 1)
+    [S,N, f] = spectral.mtspec(y, params)
+    [pspec,vpspec,f] = spectral.bootfunc(y, 100, 50, params, func = 'pspec')
+    Slist[k] = max(S[abs(f - fsig) < 1])
     pspeclist[k] = max(pspec[abs(f - fsig) < 1]) 
     
     
-pl.plot(noisefac_list,Srawlist,linewidth = 2)
-pl.figure()
-pl.plot(noisefac_list,pspeclist,'k',linewidth = 2)
+pl.plot(noisefac_list,Slist,'b',linewidth = 2)
+pl.hold(True)
+pl.plot(noisefac_list,pspeclist**0.5,'r',linewidth = 2)
+pl.ylim(0,1e-4)
 pl.show()
