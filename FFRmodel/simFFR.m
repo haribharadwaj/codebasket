014429e3835bf96b7b071a87f7258eb412691619
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2013-12-16 00:42:59 -0500

    Trying better simulation parameters for FFR

--- FFRmodel/simFFR.m
+++ FFRmodel/simFFR.m
@@ -6,7 +6,7 @@ fc = 4000;
 fm = 100;
 m = 0.5;
 bw = 200;
-dur = 0.4;
+dur = 1.0;
 rise = 0.025;
 phi = 0;
 isi = 0.025;
@@ -18,7 +18,7 @@ for fibertype = 1:3; % 1- LS, 2- MS, 3-HS
     fib = fiblist{fibertype};
     fprintf(1,'\n ------------Simulating %s fibers :)-----------\n',fib);
     SR = [0.1, 15, 100];
-    stimdb = 75;
+    stimdb = 80;
     
     
     notch = 1;
@@ -26,7 +26,7 @@ for fibertype = 1:3; % 1- LS, 2- MS, 3-HS
     Ric = 0;
     Rcn = 0;
     
-    Ntrials = 20;
+    Ntrials = 1000;
     pin = maketranstone(fc,fm,m,bw,fs,dur,rise,phi);
     stimrms = sqrt(mean(pin.^2));
     
@@ -34,8 +34,8 @@ for fibertype = 1:3; % 1- LS, 2- MS, 3-HS
         fprintf (1,'\n########### Doing Trial # %d/%d ############\n',...
             trial, Ntrials);
         if(notch)
-            SNR = 50;
-            noise = makeNotchNoiseFFT(800,0,fc,dur,fs,rise,0)';
+            SNR = 30;
+            noise = makeNotchNoiseFFT(400,0,fc,dur,fs,rise,0)';
             noiserms = sqrt(mean(noise.^2));
             noise = (noise*stimrms/noiserms)*db2mag(-1*SNR);
             pin = pin + noise;
@@ -85,25 +85,26 @@ for fibertype = 1:3; % 1- LS, 2- MS, 3-HS
     Rcn = Rcn/Ntrials;
     synout = synout/Ntrials;
     
-    f_step = 25;
-    f_uniform = min(f):f_step:max(f);
-    
-    if(plotting)
-        fprintf(1, '\nAll done! Plotting... Hold on!\n');
-    end
-    % Image plotting requires uniform grid, hence interpolating
-    [tgrid,fgrid_orig] = meshgrid(t,f);
     
     % Gate out the onset response foor visualization
     t_gated = t(t>0.050);
+    resp = mean(Ric,1);
+    [Fresp,fplot] = pmtm(resp(t>0.05),1.25,[],fs);
     
-    [tgrid_new, fgrid] = meshgrid(t_gated,f_uniform);
-    synout_new = interp2(tgrid,fgrid_orig,synout, tgrid_new, fgrid,'spline');
-    Ric_new = interp2(tgrid,fgrid_orig,Ric, tgrid_new, fgrid,'spline');
-    Rcn_new = interp2(tgrid,fgrid_orig,Rcn, tgrid_new, fgrid,'spline');
-    resp = mean(Ric_new,1);
-    [Fresp,fplot] = pmtm(resp,2,[],fs);
     if(plotting)
+        fprintf(1, '\nAll done! Plotting... Hold on!\n');
+        
+        % Image plotting requires uniform grid, hence interpolating
+        [tgrid,fgrid_orig] = meshgrid(t,f);
+        
+        
+        
+        [tgrid_new, fgrid] = meshgrid(t_gated,f_uniform);
+        synout_new = interp2(tgrid,fgrid_orig,synout, tgrid_new, fgrid,'spline');
+        Ric_new = interp2(tgrid,fgrid_orig,Ric, tgrid_new, fgrid,'spline');
+        Rcn_new = interp2(tgrid,fgrid_orig,Rcn, tgrid_new, fgrid,'spline');
+        
+        
         figure;
         subplot(2,2,1);
         imagesc(t_gated,f_uniform,synout_new,...
@@ -147,13 +148,12 @@ for fibertype = 1:3; % 1- LS, 2- MS, 3-HS
     pop_resp(fibertype,:) = resp;
     pop_resp_freq(fibertype,:) = Fresp;
 end
-saveResults = 0;
+saveResults = 1;
 
-suffix = clock;
 if(saveResults)
-    fname = strcat('SimFFR_results_SNR_',num2str(SNR),...
-        '_m',num2str(m*100),'_',num2str(stimdb),'dB_',num2str(suffix(6)),'.mat');
-    save(fname,'t_gated','pop_resp','m', 'stimdb','SNR','pop_resp_freq','Ntrials');
+    fname = strcat('RESULTS/SimFFR_results_SNR_',num2str(SNR),...
+        '_m',num2str(m*100),'_',num2str(stimdb),'dB_',datestr(clock,'mmm_dd_yyyy_HH_MM'),'.mat');
+    save(fname,'t_gated','fplot','pop_resp','m', 'stimdb','SNR','pop_resp_freq','Ntrials');
 end
 
 c = 0;
