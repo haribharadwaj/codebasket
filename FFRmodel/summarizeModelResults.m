0debc40042d279743f29e17ce00feda0ad042c3d
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2013-12-17 23:00:15 -0500

    ENH: Adding summary plotting routines for FFR model

--- FFRmodel/summarizeModelResults.m
+++ FFRmodel/summarizeModelResults.m
@@ -2,13 +2,29 @@
 clear all;
 close all;
 clc;
-flist = dir('./RESULTS/*.mat');
 
-Nsims = numel(flist);
-for j = 1:Nsims
-    fprintf(1,'Reading File # %d / %d\n',j,Nsims);
-    fname = strcat('./RESULTS/',flist(j).name);   
-    [d(j,:),p(j,:),level(j),m(j)] = getResults(fname);
-end
-
+if(~exist('resultsSummary.mat','file'))
+    flist = dir('./RESULTS/*.mat');
     
+    Nsims = numel(flist);
+    for j = 1:Nsims
+        fprintf(1,'Reading File # %d / %d\n',j,Nsims);
+        fname = strcat('./RESULTS/',flist(j).name);
+        [d(j,:),p(j,:),level(j),m(j)] = getResults(fname);
+    end
+else
+    load('./resultsSummary.mat');
+    
+    
+    figure;
+    lev80_ind = (level == 80);
+    plot(d(1,:),p(lev80_ind,:),'o-','linew',2)
+    legend(num2str(m(lev80_ind)))
+    legend(num2str(m(lev80_ind)'))
+    set(gca,'FontSize',20);
+    figure;
+    m60_ind = (m == 60);
+    plot(d(1,:),p(m60_ind,:),'o-','linew',2)
+    legend(num2str(level(m60_ind)'))
+    set(gca,'FontSize',20);
+end
