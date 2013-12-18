abdd358bdcaaee60756d53faa44998b53a74ab42
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2013-12-17 22:42:48 -0500

    FIX: Bug fixes in results summarization script

--- FFRmodel/summarizeModelResults.m
+++ FFRmodel/summarizeModelResults.m
@@ -5,14 +5,10 @@ clc;
 flist = dir('./RESULTS/*.mat');
 
 Nsims = numel(flist);
-d = zeros(1,Nsims);
-p = zeros(1,Nsims);
-level = zeros(1,Nsims);
-m = zeros(1,Nsims);
 for j = 1:Nsims
     fprintf(1,'Reading File # %d / %d\n',j,Nsims);
     fname = strcat('./RESULTS/',flist(j).name);   
-    [d(j),p(j),level(j),m(j)] = getResults(fname);
+    [d(j,:),p(j,:),level(j),m(j)] = getResults(fname);
 end
 
     
