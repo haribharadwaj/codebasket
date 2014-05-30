98e2b3cf7d713324bc8ef7c7130eda5f2964fd03
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2014-05-30 14:23:20 -0400

    Plotting more things for 40 Hz data

--- SSRITD/SSRsummary.py
+++ SSRITD/SSRsummary.py
@@ -9,9 +9,10 @@ from scipy import io, linalg
 import pylab as pl
 
 def plotSSRResults(subjlist, what,summary = False, max = False,
-                     ch_sel = 30):
+                     ch_sel = 30, OnlyReturn = True):
         
-    froot = '/home/hari/Documents/PythonCodes/research/SSRITD/EEG/'   
+    #froot = '/home/hari/Documents/PythonCodes/research/SSRITD/EEG/'   
+    froot = '/home/hari/Documents/SSRresults/'
     
     nsubjs = len(subjlist)
     
@@ -43,19 +44,21 @@ def plotSSRResults(subjlist, what,summary = False, max = False,
                 whatever = dat[what][ch_sel,f_ind]
             
         whatever_all[k] = whatever
-        pl.plot(20*np.log10(whatever/1e-6),'o-',
-                linewidth = 2)    
-        pl.ylabel(what + ' (dB re: 1 uV)', fontsize = 20)
-        pl.xlabel('Modulation Depth',fontsize = 20)
-        pl.hold(True)
-        ax = pl.gca()
-        for tick in ax.xaxis.get_major_ticks():
-            tick.label1.set_fontsize(20)
-        for tick in ax.yaxis.get_major_ticks():
-            tick.label1.set_fontsize(20)    
-        
-    pl.show()
-    pl.legend(subjlist)        
+        if not OnlyReturn:
+            pl.plot(20*np.log10(whatever/1e-6),'o-',
+                    linewidth = 2)    
+            pl.ylabel(what + ' (dB re: 1 uV)', fontsize = 20)
+            pl.xlabel('Modulation Depth',fontsize = 20)
+            pl.hold(True)
+            ax = pl.gca()
+            for tick in ax.xaxis.get_major_ticks():
+                tick.label1.set_fontsize(20)
+            for tick in ax.yaxis.get_major_ticks():
+                tick.label1.set_fontsize(20)    
+            
+            pl.show()
+            pl.legend(subjlist)   
+            
     return  whatever_all
     
 
@@ -63,6 +66,14 @@ def plotSSRResults(subjlist, what,summary = False, max = False,
 subjlist = ['I02','I03','I05','I06','I08','I09','I11','I13','I14','I15','I17',
             'I18','I19','I20','I25','I26','I29','I30','I33','I35','I36','I37'] 
   
+what = 'N'
+N = plotSSRResults(subjlist,what)
 what = 'cplv'
 cplv = plotSSRResults(subjlist,what)
-            
+what = 'S'
+S = plotSSRResults(subjlist,what)
+what = 'Sraw'
+Sraw = plotSSRResults(subjlist,what)
+
+what = 'pS'
+pS = plotSSRResults(subjlist,what)
