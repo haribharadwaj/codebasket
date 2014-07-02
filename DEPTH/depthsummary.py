6b158ae129dc076cfb565b1d57f1790bfb2aac52
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2014-07-02 12:03:11 -0400

    Nicer depth summary

--- DEPTH/depthsummary.py
+++ DEPTH/depthsummary.py
@@ -12,8 +12,8 @@ import pylab as pl
 def plotDepthResults(subjlist, what, numCondsToPlot, summary=False, max=False,
                      ch_sel=30):
 
-    froot = '/home/hari/Documents/PythonCodes/research/DepthResults/'
-    #froot = '/home/hari/Documents/DepthResults/'
+    #froot = '/home/hari/Documents/PythonCodes/research/DepthResults/'
+    froot = '/home/hari/Documents/DepthResults/'
 
     nsubjs = len(subjlist)
     condlist = [[1, 7], [2, 8], [3, 9], [4, 10]]
@@ -71,10 +71,10 @@ def plotDepthResults(subjlist, what, numCondsToPlot, summary=False, max=False,
 
 # I36 was recorded with wrong sampling rate, I16 is missing
 top = ['I37', 'I15', 'I39', 'I26', 'I03', 'I13', 'I33', 'I19', 'I02',
-       'I30', 'I41', 'I14', 'I08']
+       'I30', 'I41', 'I14', 'I08', 'I36']
 
 bottom = ['I09', 'I18', 'I25', 'I05', 'I29',
-          'I20', 'I36', 'I06', 'I11', 'I07', 'I05']
+          'I20', 'I36', 'I06', 'I11', 'I07']
 
 what = 'S'
 numCondsToPlot = 3
@@ -82,11 +82,11 @@ S_top = np.log10(plotDepthResults(top, what, numCondsToPlot)/1e-6) * 20.0
 S_bottom = np.log10(plotDepthResults(bottom, what, numCondsToPlot)/1e-6) * 20.0
 
 m = [0, -4.0, -8.0, -12.0]
-top_mu = S_top.mean(axis=0)
-top_err = S_top.std(axis=0)/(len(top)**0.5)
+top_mu = S_top[:5, :].mean(axis=0)
+top_err = S_top[:5, :].std(axis=0)/(len(top)**0.5)
 
-bottom_mu = S_bottom.mean(axis=0)
-bottom_err = S_bottom.std(axis=0)/(len(bottom)**0.5)
+bottom_mu = S_bottom[-5:, :].mean(axis=0)
+bottom_err = S_bottom[-5:, :].std(axis=0)/(len(bottom)**0.5)
 
 pl.close('all')
 pl.figure()
@@ -97,16 +97,16 @@ pl.plot(m, S_top.T, '--', linewidth=2, color=[0.5, 0.5, 0.5])
 pl.plot(m, S_bottom.T, '--', linewidth=2, color=[0.5, 0.5, 0.5])
 pl.xlabel('Modulation Depth (dB re: 100%)', fontsize=20)
 pl.ylabel('EFR magnitude (dB re: 1uV)', fontsize=20)
-pl.xlim((-8.5, 0.5))
+pl.xlim((-12.1, 0.1))
 pl.show()
 
 allsubj = top + bottom
 S_all = np.log10(plotDepthResults(allsubj, what, numCondsToPlot)/1e-6) * 20
 N_all = np.log10(plotDepthResults(allsubj, 'N', numCondsToPlot)/1e-6) * 20
 all_mu = S_all.mean(axis=0)
-all_err = S_all.std(axis=0)/(len(allsubj)**0.5)
+all_err = S_all.std(axis=0)  # / (len(allsubj)**0.5)
 all_mu_n = N_all.mean()
-all_err_n = N_all.std()/(len(allsubj)**0.5)
+all_err_n = N_all.std() / (len(allsubj)**0.5)
 pl.close('all')
 pl.figure()
 pl.errorbar(m, all_mu, yerr=all_err, linewidth=3, color='k', marker='s', ms=10)
@@ -119,9 +119,11 @@ ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 ax.fill_between(np.arange(-15, 2),
                 all_mu_n.repeat(17) + 1.645*all_err_n.repeat(17),
-                all_mu_n.repeat(17) - 10, facecolor='grey', alpha=0.25)
+                all_mu_n.repeat(17) - 10, facecolor='grey', alpha=0.4)
 pl.hold(True)
-pl.plot(m, S_all.T, '--', linewidth=2, color=[0.5, 0.5, 0.5])
+quartilemedians = [1, 5, 6, 10]
+pl.plot(m, S_all[quartilemedians, :].T, '--', linewidth=2,
+        color=[0.5, 0.5, 0.5])
 pl.xlim((-12.1, 0.1))
 pl.ylim((-15.0, 15.0))
 pl.show()
