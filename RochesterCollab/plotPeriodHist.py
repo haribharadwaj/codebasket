06db5e83957508707603a78c2aa4cb0c27232c12
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2014-07-22 09:45:13 -0400

    Adding code to do period histograms using data from the Carney lab

---
+++ RochesterCollab/plotPeriodHist.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 17 09:46:47 2014
+
+@author: hari
+"""
+import numpy as np
+from scipy import io
+import pylab as pl
+
+
+froot = '/home/hari/Documents/PythonCodes/research/CarneyCollab/'
+bf = '4k'
+
+SNRs = map(str, [1, 2, 3])
+actualSNR = [20, 35, 50]
+
+for k, SNR in enumerate(SNRs):
+    fname = 'Hari_' + bf + 'cell_SNR' + SNR + 'data.mat'
+    dat = io.loadmat(froot + fname)
+    x = np.squeeze(dat['x']/1000.0)
+    y = np.squeeze(dat['y'])
+    pl.figure(num=1)
+    pl.subplot(3, 1, k+1)
+    pl.plot(x, y, '.k', markersize=8)
+    pl.xlim((0, 500))
+    pl.ylabel('Trial #', fontsize=20)
+    pl.title('SNR = ' + str(actualSNR[k]) + ', BF = ' + bf, fontsize=20)
+
+    x = x[np.logical_and(x > 25, x < 500)]
+    nfolds = np.round((500-25)/10)
+    x_fold = np.remainder(x, 10.0)
+    nbins = 15
+    count, edges = np.histogram(x_fold, bins=nbins)
+    binsize = np.mean(np.diff(edges))/1000.0
+    cyc = (edges[1:] + edges[:-1])*0.5/10.0
+    pl.figure(num=2)
+    pl.subplot(3, 1, k+1)
+    ntrials = 100
+    pl.plot(cyc, count / (binsize * ntrials * nfolds), 'ko-', linewidth=2)
+    pl.ylim((2, 30))
+    pl.ylabel('Rate', fontsize=20)
+    pl.title('SNR = ' + str(actualSNR[k]) + ', BF = ' + bf, fontsize=20)
+    vs = np.abs(np.sum(count*np.exp(1j*cyc*2*np.pi)))/count.sum()
+    vecstr = 'Vector strength = ' + str(np.round(vs, decimals=3))
+    pl.text(0.6, 25, vecstr)
+
+pl.figure(num=1)
+pl.xlabel('PST (ms)', fontsize=20)
+pl.show()
+
+pl.figure(num=2)
+pl.xlabel('Period', fontsize=20)
+pl.show()
