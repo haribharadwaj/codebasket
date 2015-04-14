1b599f0dc8b4c1365db2f192607f61166bf7c9ef
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2015-04-14 17:45:03 -0400

    More edits to ATEEG analysis.. combining left and right

--- ATTEEG/ATTEEGanalysis.py
+++ ATTEEG/ATTEEGanalysis.py
@@ -9,15 +9,16 @@ from mne.preprocessing.ssp import compute_proj_epochs
 from mne.time_frequency import tfr_multitaper
 
 # Adding Files and locations
-froot = '/Users/Hari/Documents/Data/ATTEEG/'
+# froot = '/Users/Hari/Documents/Data/ATTEEG/'
+froot = '/home/hari/Documents/PythonCodes/ATTEEG/'
 
 # List of files stems, each will be appended by run number
 # Could be different for edf, fiff, eve etc.
 # Use list [] and enumerate over if filenames are weird
 
-subjlist = ['I14', ]
+subjlist = ['I33', ]
 
-ch = [3, 4, 25, 26, 30, 31]  # Channels of interest
+ch = [3, 4, 25, 26, 30, 31]  # Channels of interest for auditory ERPs
 
 for k, subj in enumerate(subjlist):
     fpath = froot + subj + '/'
@@ -30,14 +31,14 @@ for k, subj in enumerate(subjlist):
     bdfs = fnmatch.filter(os.listdir(fpath), subj + '*.bdf')
 
     if len(bdfs) > 1:
-        print '***WARNING!! Multiple files found! Cannot continue!***'
-    else:
-        print 'Viola! Data files found!'
-        edfname = bdfs[0]
+        print '***WARNING!! Multiple files found!***'
+
+    print 'Viola! Data files found!'
+    edfname = bdfs[0]
 
     # Load data and read event channel
     (raw, eves) = bs.importbdf(fpath + edfname, refchans=None)
-    raw.info['bads'] += ['A7', 'A24']
+    raw.info['bads'] += ['A7', 'A24', 'A28', 'EXG1', 'EXG2']
 
     # Filter the data for ERPs
     raw.filter(l_freq=0.5, h_freq=200, l_trans_bandwidth=0.15,
@@ -52,21 +53,15 @@ for k, subj in enumerate(subjlist):
     raw.add_proj(blink_projs)
 
     # Epoching and averaging
-
-    epochsL = mne.Epochs(raw, eves, 3, tmin=-0.5, proj=True,
-                         tmax=2.5, baseline=(-0.5, 0.0),
-                         reject=dict(eeg=200e-6))
-    epochsR = mne.Epochs(raw, eves, 5, tmin=-0.5, proj=True,
-                         tmax=2.5, baseline=(-0.5, 0.0),
-                         reject=dict(eeg=200e-6))
+    epochs = mne.Epochs(raw, eves, [3, 5], tmin=-0.5, proj=True,
+                        tmax=6.0, baseline=(-0.5, 0.0),
+                        reject=dict(eeg=200e-6))
 
 freqs = np.arange(2, 100, 2)  # define frequencies of interest
 n_cycles = freqs / float(5)  # different number of cycle per frequency
 n_cycles[freqs < 5] = 1
-powerL, itcL = tfr_multitaper(epochsL, freqs=freqs, n_cycles=n_cycles,
-                              time_bandwidth=2.0, n_jobs=-1)
-powerR, itcR = tfr_multitaper(epochsR, freqs=freqs, n_cycles=n_cycles,
-                              time_bandwidth=2.0, n_jobs=-1)
-powerL_R = powerL - powerR
-powerL_R.plot_topo(baseline=(-0.5, 0.), mode='zscore', dB=False, vmin=-20.,
-                   vmax=20.)
+# epochs.subtract_evoked()
+power, itc = tfr_multitaper(epochs, freqs=freqs, n_cycles=n_cycles,
+                            time_bandwidth=2.0, n_jobs=-1)
+power.data = 20*np.log10(power.data)
+power.plot_topo(dB=False, baseline=(-0.5, 0.))
