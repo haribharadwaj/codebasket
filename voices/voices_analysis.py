c0a05f338459b7d0987ec4394cb601d3b4f938b4
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2015-06-23 16:48:48 -0400

    Adding preproc improvements (transform to run1 coildef)  and saving of intermediate files for speech paradigm

--- voices/voices_analysis.py
+++ voices/voices_analysis.py
@@ -13,7 +13,6 @@ froot = '/autofs/cluster/transcend/hari/voices/'
 subjlist = ['083701', ]
 para = 'voices'
 epochs = []
-
 for subj in subjlist:
 
     fpath = froot + subj + '/'
@@ -28,15 +27,17 @@ for subj in subjlist:
     raw = mne.io.Raw(fifs, preload=True)
     eves = mne.find_events(raw, stim_channel='STI101', shortest_event=1)
 
-    # raw.info['bads'] += ['MEG2312', 'MEG2613', 'MEG0212', 'MEG1622']
     # Filter the data for ERPs
     raw.filter(l_freq=0.5, h_freq=144.0, l_trans_bandwidth=0.15,
                picks=np.arange(0, 306, 1))
 
     # raw.apply_proj()
     fs = raw.info['sfreq']
+
     # SSP for blinks
     blinks = find_blinks(raw, ch_name='EOG062')
+    blinkname = fpath + subj + '_' + para + '_blinks_sss-eve.fif'
+    mne.write_events(blinkname, blinks)
     epochs_blinks = mne.Epochs(raw, blinks, 998, tmin=-0.25,
                                tmax=0.25, proj=True,
                                baseline=(-0.25, 0),
@@ -46,6 +47,25 @@ for subj in subjlist:
                                       n_mag=2, n_eeg=0,
                                       verbose='DEBUG')
     raw.add_proj(blink_projs)
+
+    # SSP for cardiac
+    ekg_name = 'ECG063' if 'ECG063' in raw.ch_names else 'MEG1421'
+    ekg_thresh = 300e-6 if 'ECG063' in raw.ch_names else 1.5e-12
+    qrs = find_blinks(raw, ch_name=ekg_name, h_freq=100.0,
+                      thresh=ekg_thresh, event_id=999)
+    qrsname = fpath + subj + '_' + para + '_qrs_sss-eve.fif'
+    mne.write_events(qrsname, qrs)
+    epochs_qrs = mne.Epochs(raw, qrs, 999, tmin=-0.1,
+                            tmax=0.1, proj=True,
+                            baseline=(-0.1, 0),
+                            reject=dict(grad=8000e-13,
+                                        mag=8e-12))
+    qrs_projs = compute_proj_epochs(epochs_qrs, n_grad=2,
+                                    n_mag=2, n_eeg=0,
+                                    verbose='DEBUG')
+    raw.add_proj(qrs_projs)
+    useProj = True
+
     evokeds = []
     condlists = [1, 2, 3, 4, 5, 6, 7, 8]
     condnames = ['SpeechA', 'SpeechB', 'JabberA', 'JabberB',
@@ -56,7 +76,7 @@ for subj in subjlist:
         print 'Running Subject', subj, 'Condition', condstem
 
         # Epoching events of type
-        epochs = mne.Epochs(raw, eves, condlist, tmin=-0.5, proj=True,
+        epochs = mne.Epochs(raw, eves, condlist, tmin=-0.5, proj=useProj,
                             tmax=2.0, baseline=(-0.3, 0.0), name=condstem,
                             reject=dict(grad=5000e-13, mag=5e-12))
         epo_name = fpath + subj + '_' + para + '_' + condstem + '-epo.fif'
--- voices/voices_sss.py
+++ voices/voices_sss.py
@@ -6,9 +6,9 @@ import os
 # froot = '/home/hari/Documents/PythonCodes/voices/'
 froot = '/autofs/cluster/transcend/hari/voices/'
 
-subjlist = ['083701', ]
+subjlist = ['053001', ]
 paradigm = 'voices'
-
+hp_est = True
 for subj in subjlist:
 
     fpath = froot + subj + '/'
@@ -24,8 +24,22 @@ for subj in subjlist:
         frame = 'head'
         logname = fpath + subj + '_' + paradigm + ('_' + str(k+1) +
                                                    '_maxfilter.log')
-        hpname = fpath + subj + '_' + paradigm + ('_' + str(k+1) +
-                                                  '_hp.txt')
+        if hp_est:
+            hpname = (fpath + subj + '_' + paradigm + '_' + str(k+1) +
+                      '_hp.txt')
+            mv_hp = hpname
+            mv_headpos = True
+        else:
+            mv_hp = None
+            mv_headpos = False
+
+        # Transform everything to the coil definition of run 1
+        if k > 0:
+            run1name = fpath + subj + '_' + paradigm + ('_1_raw_sss.fif')
+            mv_trans = run1name
+        else:
+            mv_trans = None
+
         mx_args = '-in 9 -out 3 -v | tee ' + logname
         badchname = fpath + 'badch.txt'
         if os.path.isfile(badchname):
@@ -36,8 +50,54 @@ for subj in subjlist:
 
         # Calling maxfiler
         origin = apply_maxfilter(fpath + rawname, sssname, frame=frame,
-                                 bad=bads, mv_hp=hpname, mv_comp='inter',
-                                 mv_hpicons=True, mx_args=mx_args,
+                                 bad=bads, mv_hp=mv_hp, mv_comp='inter',
+                                 mv_headpos=mv_headpos, mv_hpicons=True,
+                                 mv_trans=mv_trans, mx_args=mx_args,
+                                 verbose='DEBUG')
+        print 'Estimated head center was ', origin
+        print '----- YAY! Done with subject ', subj
+
+    # Do overflow files... doing only 1 for now
+    fifs = fnmatch.filter(os.listdir(fpath), subj + '*raw-1.fif')
+    print 'Viola!', len(fifs),  'files found!'
+    if len(fifs) > 1:
+        nruns = len(fifs)
+        print 'WARNING! Multiple raw files found'
+    else:
+        nruns = 1
+
+    for k, rawname in enumerate(fifs):
+        sssname = fpath + subj + '_' + paradigm + ('_' + str(k+1) +
+                                                   '_raw_sss-1.fif')
+        # Maxfilter parameters
+        frame = 'head'
+        logname = fpath + subj + '_' + paradigm + ('_' + str(k+1) +
+                                                   '_maxfilter-1.log')
+        if hp_est:
+            hpname = (fpath + subj + '_' + paradigm + '_' + str(k+1) +
+                      '_hp-1.txt')
+            mv_hp = hpname
+            mv_headpos = True
+        else:
+            mv_hp = None
+            mv_headpos = False
+
+        # Transform everything to the coil definition of run 1
+        run1name = fpath + subj + '_' + paradigm + ('_1_raw_sss.fif')
+        mv_trans = run1name
+
+        mx_args = '-in 9 -out 3 -v | tee ' + logname
+        badchname = fpath + 'badch.txt'
+        if os.path.isfile(badchname):
+            bads = open(badchname, 'r').read().strip('\n')
+        else:
+            print 'Sorry.. bad channel list not found! skipping subject'
+            continue
+        # Calling maxfiler
+        origin = apply_maxfilter(fpath + rawname, sssname, frame=frame,
+                                 bad=bads, mv_hp=mv_hp, mv_comp='inter',
+                                 mv_headpos=mv_headpos, mv_hpicons=True,
+                                 mv_trans=mv_trans, mx_args=mx_args,
                                  verbose='DEBUG')
         print 'Estimated head center was ', origin
         print '----- YAY! Done with subject ', subj
