0eed6e5afa4e7c41b40a25bf1ac844a3549f4015
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2015-06-29 15:29:25 -0400

    Fixing voices SSS for trans to be done separately and on the correct files

--- voices/voices_sss.py
+++ voices/voices_sss.py
@@ -57,46 +57,51 @@ for subj in subjlist:
         print 'Estimated head center was ', origin
 
     # Do overflow files... doing only 1 for now
-    fifs = fnmatch.filter(os.listdir(fpath), subj + '*raw-1.fif')
-    print 'Viola!', len(fifs),  'files found!'
-    if len(fifs) > 1:
-        nruns = len(fifs)
-        print 'WARNING! Multiple raw files found'
-    else:
-        nruns = 1
-
-    for k, rawname in enumerate(fifs):
-        transtag = '_notrans'
-        sssname = fpath + subj + '_' + paradigm + ('_' + str(k+1) + transtag +
-                                                   '_raw_sss-1.fif')
-        # Maxfilter parameters
-        frame = 'head'
-        logname = fpath + subj + '_' + paradigm + ('_' + str(k+1) + transtag +
-                                                   '_maxfilter-1.log')
-        if hp_est:
-            hpname = (fpath + subj + '_' + paradigm + '_' + str(k+1) +
-                      transtag + '_hp-1.txt')
-            mv_hp = hpname
-            mv_headpos = True
+    for run in range(1, 3):
+        fifs = fnmatch.filter(os.listdir(fpath), subj + '*' +
+                              str(run) + '_raw-1.fif')
+        fifs = fnmatch.filter(os.listdir(fpath), subj + '*raw-1.fif')
+        print 'Viola!', len(fifs),  'files found!'
+        if len(fifs) > 1:
+            nruns = len(fifs)
+            print 'WARNING! Multiple raw files found'
         else:
-            mv_hp = None
-            mv_headpos = False
+            nruns = 1
 
-        mv_trans = None
-        mx_args = '-in 9 -out 3 -v | tee ' + logname
-        badchname = fpath + 'badch.txt'
-        if os.path.isfile(badchname):
-            bads = open(badchname, 'r').read().strip('\n')
-        else:
-            print 'Sorry.. bad channel list not found! skipping subject'
-            continue
-        # Calling maxfiler
-        origin = apply_maxfilter(fpath + rawname, sssname, frame=frame,
-                                 bad=bads, mv_hp=mv_hp, mv_comp='inter',
-                                 mv_headpos=mv_headpos, mv_hpicons=True,
-                                 mv_trans=mv_trans, mx_args=mx_args,
-                                 verbose='DEBUG')
-        print 'Estimated head center was ', origin
+        for k, rawname in enumerate(fifs):
+            transtag = '_notrans'
+            sssname = fpath + subj + '_' + paradigm + ('_' + str(run) +
+                                                       transtag +
+                                                       '_raw_sss-1.fif')
+            # Maxfilter parameters
+            frame = 'head'
+            logname = fpath + subj + '_' + paradigm + ('_' + str(run) +
+                                                       transtag +
+                                                       '_maxfilter-1.log')
+            if hp_est:
+                hpname = (fpath + subj + '_' + paradigm + '_' + str(run) +
+                          transtag + '_hp-1.txt')
+                mv_hp = hpname
+                mv_headpos = True
+            else:
+                mv_hp = None
+                mv_headpos = False
+
+            mv_trans = None
+            mx_args = '-in 9 -out 3 -v | tee ' + logname
+            badchname = fpath + 'badch.txt'
+            if os.path.isfile(badchname):
+                bads = open(badchname, 'r').read().strip('\n')
+            else:
+                print 'Sorry.. bad channel list not found! skipping subject'
+                continue
+            # Calling maxfiler
+            origin = apply_maxfilter(fpath + rawname, sssname, frame=frame,
+                                     bad=bads, mv_hp=mv_hp, mv_comp='inter',
+                                     mv_headpos=mv_headpos, mv_hpicons=True,
+                                     mv_trans=mv_trans, mx_args=mx_args,
+                                     verbose='DEBUG')
+            print 'Estimated head center was ', origin
 
 
 # Separately run --trans to the coil definitions of run 1
@@ -122,34 +127,36 @@ for subj in subjlist:
             logname = fpath + subj + '_' + paradigm + ('_' + str(k+1) +
                                                        '_trans_maxfilter.log')
             # Calling maxfiler
-            origin = apply_maxfilter(fpath + sss_old, sssname, frame=frame,
+            origin = apply_maxfilter(sss_old, sssname, frame=frame,
                                      mv_trans=mv_trans, mx_args=mx_args,
                                      verbose='DEBUG')
             print 'Estimated head center was ', origin
             print '----- YAY! Done with subject ', subj
 
     # Do overflow files... doing only 1 for now
-    fifs = fnmatch.filter(os.listdir(fpath), subj + '*raw-1.fif')
-    for k, rawname in enumerate(fifs):
-        sss_old = fpath + subj + '_' + paradigm + ('_' + str(k+1) +
-                                                   '_notrans_raw_sss-1.fif')
-        sssname = fpath + subj + '_' + paradigm + ('_' + str(k+1) +
-                                                   '_raw_sss-1.fif')
-        # Maxfilter parameters
-        frame = 'head'
-        logname = fpath + subj + '_' + paradigm + ('_' + str(k+1) +
-                                                   '_trans_maxfilter-1.log')
+    for run in range(1, 3):
+        fifs = fnmatch.filter(os.listdir(fpath), subj + '*' +
+                              str(run) + '_raw-1.fif')
+        for k, rawname in enumerate(fifs):
+            sss_old = fpath + subj + '_' + paradigm + ('_' + str(run) + '_no'
+                                                       'trans_raw_sss-1.fif')
+            sssname = fpath + subj + '_' + paradigm + ('_' + str(run) +
+                                                       '_raw_sss-1.fif')
+            # Maxfilter parameters
+            frame = 'head'
+            logname = fpath + subj + '_' + paradigm + ('_' + str(run) + '_'
+                                                       'trans_maxfilter-1.log')
 
-        if k > 0:
-            # Transform everything to the coil definition of run 1
-            run1name = fpath + subj + '_' + paradigm + ('_1_raw_sss.fif')
-            mv_trans = run1name
+            if k > 0:
+                # Transform everything to the coil definition of run 1
+                run1name = fpath + subj + '_' + paradigm + ('_1_raw_sss.fif')
+                mv_trans = run1name
 
-            mx_args = '-in 9 -out 3 -v | tee ' + logname
+                mx_args = '-in 9 -out 3 -v | tee ' + logname
 
-            # Calling maxfiler
-            origin = apply_maxfilter(fpath + sss_old, sssname, frame=frame,
-                                     mv_trans=mv_trans, mx_args=mx_args,
-                                     verbose='DEBUG')
-            print 'Estimated head center was ', origin
-            print '----- YAY! Done with subject ', subj
+                # Calling maxfiler
+                origin = apply_maxfilter(sss_old, sssname, frame=frame,
+                                         mv_trans=mv_trans, mx_args=mx_args,
+                                         verbose='DEBUG')
+                print 'Estimated head center was ', origin
+                print '----- YAY! Done with subject ', subj
