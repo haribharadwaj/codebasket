6bd48fa4062732ddff18714ce56427af4f0d9478
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2015-07-08 23:03:03 -0400

    Modified ABR scripts for MEMR analyses

--- MEMR/ABR_MEMR_32Channel.py
+++ MEMR/ABR_MEMR_32Channel.py
@@ -19,7 +19,7 @@ for subj in subjlist:
     print 'Running Subject', subj
 
     bdfs = fnmatch.filter(os.listdir(fpath), subj + '_' + ear +
-                          '_ABR_module.bdf')
+                          '_ABR.bdf')
 
     if len(bdfs) > 1:
         print 'Warning! More than 1 file found!'
@@ -27,31 +27,33 @@ for subj in subjlist:
         edfname = bdfs[0]
 
     # Load data and read event channel
-    (raw, eves) = bs.importbdf(fpath + edfname, nchans=2, refchans=None)
+    if ear == 'Right':
+        refchans = ['EXG2', 'EXG1']
+    else:
+        refchans = ['EXG1', ]
+    (raw, eves) = bs.importbdf(fpath + edfname, nchans=34, refchans=refchans)
 
     # Filter the data
-    raw.filter(l_freq=130, h_freq=2000, picks=np.arange(0, 2, 1))
+    raw.filter(l_freq=130, h_freq=2000, picks=np.arange(0, 34, 1))
     abrs = []
     pl.figure()
     for cond in conds:
         print 'Doing condition ', cond
-        epochs = mne.Epochs(raw, eves, cond, tmin=0., proj=False,
-                            tmax=0.014, baseline=(0.001, 0.002),
-                            reject=dict(eeg=20e-6), verbose='WARNING')
+        epochs = mne.Epochs(raw, eves, cond, tmin=-0.005, proj=False,
+                            tmax=0.014, baseline=(-0.005, 0.001),
+                            picks=np.arange(0, 34, 1), reject=dict(eeg=100e-6),
+                            verbose='WARNING')
         abr = epochs.average()
         abrs += [abr, ]
         x = abr.data * 1e6  # microV
         t = abr.times * 1e3 - 1.6  # Adjust for delay and use milliseconds
-        if ear == 'Right':
-            pl.plot(t, x[1, :] - x[0, :], linewidth=2)
-        else:
-            pl.plot(t, x[0, :] - x[1, :], linewidth=2)
+        pl.plot(t, -1 * x[31, :], linewidth=2)
         pl.hold(True)
 pl.xlabel('Time (ms)', fontsize=16)
 pl.ylabel('ABR (uV)', fontsize=16)
 pl.legend(('Condensation', 'Rarefaction', 'Combined'), loc='best')
-pl.xlim((0., 9.))
+pl.xlim((-1., 9.))
 ax = pl.gca()
 ax.tick_params(labelsize=16)
 pl.show()
-mne.write_evokeds(fpath + subj + '_abr-ave.fif', abrs)
+# mne.write_evokeds(fpath + subj + '_abr-ave.fif', abrs)
