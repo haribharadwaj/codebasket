da80d579b4e8faf699f4874af5ee0be9a2f9db78
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2015-07-15 14:00:16 -0400

    Code to include tiptrode

--- MEMR/ABR_MEMR_32Channel.py
+++ MEMR/ABR_MEMR_32Channel.py
@@ -7,11 +7,12 @@ import pylab as pl
 
 # Adding Files and locations
 # froot = '/cluster/transcend/hari/MEMR/'
-froot = '/Users/Hari/Documents/Data/MEMR/ABR/'
+# froot = '/Users/Hari/Documents/Data/MEMR/ABR/'
+froot = '/home/hari/Documents/PythonCodes/MEMR'
 
-subjlist = ['I50', ]
+subjlist = ['I13', ]
 ear = 'Right'
-conds = [3, 6, [3, 6]]
+conds = [[2, 5], [3, 6]]
 for subj in subjlist:
 
     fpath = froot + '/' + subj + '/'
@@ -28,31 +29,32 @@ for subj in subjlist:
 
     # Load data and read event channel
     if ear == 'Right':
-        refchans = ['EXG2', 'EXG1']
+        refchans = ['EXG2']
     else:
         refchans = ['EXG1', ]
-    (raw, eves) = bs.importbdf(fpath + edfname, nchans=34, refchans=refchans)
-
+    (raw, eves) = bs.importbdf(fpath + edfname, nchans=35, refchans=refchans)
+    raw.set_channel_types({'EXG3': 'eeg'})
     # Filter the data
-    raw.filter(l_freq=130, h_freq=2000, picks=np.arange(0, 34, 1))
+    raw.filter(l_freq=70, h_freq=2000, picks=np.arange(30, 35, 1))
     abrs = []
     pl.figure()
     for cond in conds:
         print 'Doing condition ', cond
         epochs = mne.Epochs(raw, eves, cond, tmin=-0.005, proj=False,
                             tmax=0.014, baseline=(-0.005, 0.001),
-                            picks=np.arange(0, 34, 1), reject=dict(eeg=100e-6),
+                            picks=np.arange(30, 35, 1),
+                            reject=dict(eeg=100e-6),
                             verbose='WARNING')
         abr = epochs.average()
         abrs += [abr, ]
         x = abr.data * 1e6  # microV
         t = abr.times * 1e3 - 1.6  # Adjust for delay and use milliseconds
-        pl.plot(t, -1 * x[31, :], linewidth=2)
+        pl.plot(t, x[3, :] - x[4, :], linewidth=2)
         pl.hold(True)
 pl.xlabel('Time (ms)', fontsize=16)
 pl.ylabel('ABR (uV)', fontsize=16)
-pl.legend(('Condensation', 'Rarefaction', 'Combined'), loc='best')
-pl.xlim((-1., 9.))
+pl.legend(('80 dB peSPL', '100 dB peSPL'), loc='best')
+pl.xlim((0., 9.))
 ax = pl.gca()
 ax.tick_params(labelsize=16)
 pl.show()
