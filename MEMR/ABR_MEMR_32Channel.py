5b127ae41d1d497c85627991cbeb5e6846977e7e
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2015-07-21 12:27:24 -0400

    Dealing with multiple runs for ABR

--- MEMR/ABR_MEMR_32Channel.py
+++ MEMR/ABR_MEMR_32Channel.py
@@ -10,7 +10,7 @@ import pylab as pl
 # froot = '/Users/Hari/Documents/Data/MEMR/ABR/'
 froot = '/home/hari/Documents/PythonCodes/MEMR'
 
-subjlist = ['I13', ]
+subjlist = ['I02', ]
 ear = 'Right'
 conds = [[2, 5], [3, 6]]
 for subj in subjlist:
@@ -18,30 +18,39 @@ for subj in subjlist:
     fpath = froot + '/' + subj + '/'
 
     print 'Running Subject', subj
+    rawlist = []
+    evelist = []
+    nruns = 3
+    for run in range(nruns):
+        bdfs = fnmatch.filter(os.listdir(fpath), subj + '_MEMR_' + ear +
+                              '_' + str(run + 1) + '_ABR.bdf')
 
-    bdfs = fnmatch.filter(os.listdir(fpath), subj + '_' + ear +
-                          '_ABR.bdf')
-
-    if len(bdfs) > 1:
-        print 'Warning! More than 1 file found!'
-    else:
-        edfname = bdfs[0]
-
-    # Load data and read event channel
-    if ear == 'Right':
-        refchans = ['EXG2']
-    else:
-        refchans = ['EXG1', ]
-    (raw, eves) = bs.importbdf(fpath + edfname, nchans=35, refchans=refchans)
-    raw.set_channel_types({'EXG3': 'eeg'})
-    # Filter the data
-    raw.filter(l_freq=70, h_freq=2000, picks=np.arange(30, 35, 1))
+        if len(bdfs) == 1:
+            edfname = fpath + bdfs[0]
+            # Load data and read event channel
+            if ear == 'Right':
+                refchans = ['EXG2']
+            else:
+                refchans = ['EXG1', ]
+            (rawtemp, evestemp) = bs.importbdf(edfname, nchans=35,
+                                               refchans=refchans)
+            rawtemp.set_channel_types({'EXG3': 'eeg'})
+            rawlist += [rawtemp, ]
+            evelist += [evestemp, ]
+        else:
+            if len(bdfs) == 0:
+                pass
+            else:
+                RuntimeError('More than one bdf file for same run!!')
+        # Filter the data
+        # raw.filter(l_freq=33., h_freq=3000, picks=np.arange(30, 35, 1))
+    raw, eves = mne.concatenate_raws(rawlist, events_list=evelist)
     abrs = []
     pl.figure()
     for cond in conds:
         print 'Doing condition ', cond
         epochs = mne.Epochs(raw, eves, cond, tmin=-0.005, proj=False,
-                            tmax=0.014, baseline=(-0.005, 0.001),
+                            tmax=0.014, baseline=(0., 0.002),
                             picks=np.arange(30, 35, 1),
                             reject=dict(eeg=100e-6),
                             verbose='WARNING')
@@ -49,13 +58,14 @@ for subj in subjlist:
         abrs += [abr, ]
         x = abr.data * 1e6  # microV
         t = abr.times * 1e3 - 1.6  # Adjust for delay and use milliseconds
-        pl.plot(t, x[3, :] - x[4, :], linewidth=2)
+        pl.plot(t, x[1, :] - x[4, :], linewidth=2)
         pl.hold(True)
 pl.xlabel('Time (ms)', fontsize=16)
 pl.ylabel('ABR (uV)', fontsize=16)
-pl.legend(('80 dB peSPL', '100 dB peSPL'), loc='best')
 pl.xlim((0., 9.))
 ax = pl.gca()
 ax.tick_params(labelsize=16)
+# pl.legend(('Condensation', 'Rarefaction'), loc='best')
+pl.legend(('80 dB peSPL', '100 dB peSPL'), loc='best')
 pl.show()
-# mne.write_evokeds(fpath + subj + '_abr-ave.fif', abrs)
+mne.write_evokeds(fpath + subj + '_abr-ave.fif', abrs)
