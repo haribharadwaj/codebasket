9020a2105d72fcb5c0c42e9f36c996cb91373cab
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2015-08-16 12:26:22 -0400

    Several improvements to reduce memory use and save only the important files

--- SubcorticalSource/SSSR_MEG_Localization.py
+++ SubcorticalSource/SSSR_MEG_Localization.py
@@ -4,6 +4,7 @@ import os
 import fnmatch
 from anlffr import spectral
 import pylab as pl
+from scipy import io
 
 
 def pow2db(x):
@@ -24,8 +25,9 @@ def pow2db(x):
     return y
 
     # Adding Files and locations
-froot = '/autofs/cluster/transcend/hari/MEMR/'
+# froot = '/autofs/cluster/transcend/hari/MEMR/'
 # froot = '/home/hari/Documents/SubcorticalSource/'
+froot = '/space/zazen/1/users/SubcorticalSource/'
 saveRaw = False
 subjlist = ['HB', ]
 
@@ -37,7 +39,7 @@ freqs = np.arange(5, 500, 2)  # define frequencies of interest
 n_cycles = freqs / float(3)  # different number of cycle per frequency
 n_cycles[freqs < 15] = 2
 
-cond = 3
+cond = 5
 AMlist = [163, 193, 223, 253, 283]
 for subj in subjlist:
     fpath = froot + subj + '/'
@@ -70,14 +72,14 @@ for subj in subjlist:
 
         raw.set_channel_types({'EMG061': 'eeg'})
         raw.info['bads'] += ['MEG0223', 'MEG1623']
-        raw.resample(raw.info['sfreq'] / 2.0)
+        # raw.resample(raw.info['sfreq'] / 2.0)
         # Filter the data for SSRs
         # raw.filter(l_freq=70., h_freq=300., picks=None)
 
         # Epoching events of type
         epochs = mne.Epochs(raw, eves, cond, tmin=-0.05, proj=False,
                             tmax=1.25, baseline=(-0.05, 0.),
-                            reject=dict(grad=5000e-13, mag=4e-12))
+                            reject=dict(grad=5000e-12, mag=4e-11))
 
         x = epochs.get_data()
         if saveRaw:
@@ -92,14 +94,36 @@ for subj in subjlist:
     # Fourier domain stuff
     pl.figure()
     params = dict(Fs=Fs, fpass=[100, 300], tapers=[1, 1], itc=0)
+
+    # EEG
+    pl.figure()
+    params = dict(Fs=Fs, fpass=[100, 300], tapers=[1, 1], itc=0)
+    y = x[:, eeg, :].transpose((1, 0, 2))
+    plv, f = spectral.mtplv(y, params, verbose='DEBUG')
+    pl.plot(f, plv.T, linewidth=2)
+    pl.xlabel('Frequency (Hz)', fontsize=16)
+    pl.ylabel('Intertrial PLV', fontsize=16)
+    pl.title('EEG', fontsize=16)
+    pl.xlim([100, 300])
+    pl.show()
+    figname_eeg = subj + '_' + condstem + '_eeg-results.pdf'
+    pl.savefig(fpath + figname_eeg)
+    y_eeg = y
+
+    # Magnetometers
     for badname in epochs.info['bads']:
         bad_ind = epochs.info['ch_names'].index(badname)
         if bad_ind in mags:
             mags.remove(bad_ind)
-
-    y = x[:, mags, :].transpose((1, 0, 2))
-    plv, f = spectral.mtplv(y, params, verbose='DEBUG')
-    pl.plot(f, plv.mean(axis=0), linewidth=2)
+    plv = np.zeros((len(mags), plv.shape[0]))
+    for k, ch in enumerate(mags):
+        y = x[:, [ch, ], :].transpose((1, 0, 2))
+        print 'Computing PLV for channel', k
+        plv[k, :], f = spectral.mtplv(y, params, verbose='DEBUG')
+    f_AM = AMlist[cond - 1]
+    ind_AM = np.argmin(np.abs(f - f_AM))
+    best10Chans = plv[:, ind_AM].argsort()[:-10:-1]
+    pl.plot(f, plv[best10Chans, :].mean(axis=0), linewidth=2)
     pl.xlabel('Frequency (Hz)', fontsize=16)
     pl.ylabel('Intertrial PLV', fontsize=16)
     pl.title('MEG Magnetometers', fontsize=16)
@@ -109,14 +133,15 @@ for subj in subjlist:
     pl.savefig(fpath + figname_mag)
     lout = mne.find_layout(epochs.info)
     pos = lout.pos[mags]
-    f_AM = AMlist[cond - 1]
-    ind_AM = np.argmin(np.abs(f - f_AM))
     pl.figure()
     mne.viz.plot_topomap(plv[:, ind_AM], pos, sensors='ok', vmin=-0.01,
                          vmax=0.01)
     pl.show()
     figname_mag_topo = subj + '_' + condstem + '_mag_topo-results.pdf'
     pl.savefig(fpath + figname_mag_topo)
+    y_mag = x[:, np.asarray(mags)[best10Chans], :].transpose((1, 0, 2))
+    pos_mag = pos
+    topo_mag = plv[:, ind_AM]
 
     # Gradiometers
     pl.figure()
@@ -126,9 +151,15 @@ for subj in subjlist:
         if bad_ind in grads:
             grads.remove(bad_ind)
 
-    y = x[:, grads, :].transpose((1, 0, 2))
-    plv, f = spectral.mtplv(y, params, verbose='DEBUG')
-    pl.plot(f, plv.mean(axis=0), linewidth=2)
+    plv = np.zeros((len(grads), plv.shape[1]))
+    for k, ch in enumerate(grads):
+        print 'Computing PLV for channel', k
+        y = x[:, [ch, ], :].transpose((1, 0, 2))
+        plv[k, :], f = spectral.mtplv(y, params, verbose='DEBUG')
+    f_AM = AMlist[cond - 1]
+    ind_AM = np.argmin(np.abs(f - f_AM))
+    best10Chans = plv[:, ind_AM].argsort()[:-10:-1]
+    pl.plot(f, plv[best10Chans, :].mean(axis=0), linewidth=2)
     pl.xlabel('Frequency (Hz)', fontsize=16)
     pl.ylabel('Intertrial PLV', fontsize=16)
     pl.title('MEG Gradiometers', fontsize=16)
@@ -146,17 +177,12 @@ for subj in subjlist:
     pl.show()
     figname_grad_topo = (subj + '_' + condstem + '_grad_topo-results.pdf')
     pl.savefig(fpath + figname_grad_topo)
+    y_grad = x[:, np.asarray(grads)[best10Chans], :].transpose((1, 0, 2))
+    pos_grad = pos
+    topo_grad = plv[:, ind_AM]
 
-    # EEG
-    pl.figure()
-    params = dict(Fs=Fs, fpass=[100, 300], tapers=[1, 1], itc=0)
-    y = x[:, eeg, :].transpose((1, 0, 2))
-    plv, f = spectral.mtplv(y, params, verbose='DEBUG')
-    pl.plot(f, plv.T, linewidth=2)
-    pl.xlabel('Frequency (Hz)', fontsize=16)
-    pl.ylabel('Intertrial PLV', fontsize=16)
-    pl.title('EEG', fontsize=16)
-    pl.xlim([100, 300])
-    pl.show()
-    figname_grad = subj + '_' + condstem + '_eeg-results.pdf'
-    pl.savefig(fpath + figname_grad)
+    saveDict = dict(Fs=Fs, params=params, y_mag=y_mag, y_grad=y_grad,
+                    y_eeg=y_eeg, topo_mag=topo_mag, topo_grad=topo_grad,
+                    f_AM=f_AM, t=times)
+    save_res_name = subj + '_' + condstem + '_results.mat'
+    io.savemat(fpath + save_res_name, saveDict)
