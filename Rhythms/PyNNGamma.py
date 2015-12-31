561d6beebdc9bdb0ae48d5dce037b3f6755f5320
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2015-12-31 18:40:39 -0500

    Improved spike plotting, initialization, etc.

--- Rhythms/PyNNGamma.py
+++ Rhythms/PyNNGamma.py
@@ -6,14 +6,23 @@ Created on Mon Dec 28 14:36:30 2015
 """
 import pyNN.neuron as sim
 import pylab as pl
+import numpy as np
+from pyNN.random import RandomDistribution
+
 
 sim.setup(timestep=0.01)
-
-Ecells = sim.Population(20, sim.HH_cond_exp())
-Icells = sim.Population(10, sim.HH_cond_exp())
+nE = 20
+nI = 10
+Ecells = sim.Population(nE, sim.HH_cond_exp(tau_syn_E=1.0, tau_syn_I=4.0),
+                        label='E-Cells')
+Icells = sim.Population(nI, sim.HH_cond_exp(tau_syn_E=1.0, tau_syn_I=4.0),
+                        label='I-Cells')
 Net = Ecells + Icells
+vinit_distr = RandomDistribution('uniform', (-70., -55.))
+Net.initialize(v=vinit_distr)  # Initialize to random subthreshold voltages
+
 Esyn = sim.StaticSynapse(weight=0.05, delay=1.0)
-random = sim.FixedProbabilityConnector(p_connect=0.5)
+random = sim.FixedProbabilityConnector(p_connect=0.3)
 Econnections = sim.Projection(Ecells, Net, random, Esyn,
                               receptor_type='excitatory')
 Isyn = sim.StaticSynapse(weight=0.2, delay=1.0)
@@ -23,19 +32,19 @@ Iconnections = sim.Projection(Icells, Net, random, Isyn,
 noise = sim.NoisyCurrentSource(mean=0.5, stdev=0.2, start=50.0,
                                stop=450.0, dt=1.0)
 Ecells.inject(noise)
-Net.record('v')
+Net.record(['v', 'spikes'])
 sim.run(1000.0)
 
 data = Net.get_data()
 
 sim.end()
 
-arr = data.segments[0].analogsignalarrays[0]
-
+# Plot Membrane Voltages
+V = data.segments[0].analogsignalarrays[0]
 # Plot E-cells
 ax1 = pl.subplot(211)
-for k in range(20):
-    pl.plot(arr.times, arr[:, k], linewidth=2)
+for k in range(nE):
+    pl.plot(V.times, V[:, k], linewidth=2)
     pl.hold(True)
 
 pl.xlabel('Time (ms)', fontsize=16)
@@ -46,12 +55,36 @@ pl.tick_params(labelsize=16)
 
 # Plot I-cells
 ax2 = pl.subplot(212, sharex=ax1)
-for k in range(20, 30):
-    pl.plot(arr.times, arr[:, k], linewidth=2)
+for k in range(nE, nE + nI):
+    pl.plot(V.times, V[:, k], linewidth=2)
     pl.hold(True)
 
 pl.xlabel('Time (ms)', fontsize=16)
 pl.ylabel('Membrane potential (mV)', fontsize=16)
 pl.title('I-Cells', fontsize=16)
 pl.tick_params(labelsize=16)
+pl.show()
+
+# Plot Spike Times
+spikes = data.segments[0].spiketrains
+pl.figure()
+for k in range(nE + nI):
+    spike = spikes[k]
+    col = '.k' if k < nE else '.r'
+    if k == 0:
+        lab = 'E-Cells'
+    else:
+        if k == nE:
+            lab = 'I-Cells'
+        else:
+            lab = None
+    pl.plot(spike, np.ones_like(spike) * k, col, markersize=10,
+            label=lab)
+    pl.hold(True)
+
+pl.xlabel('Time (ms)', fontsize=16)
+pl.ylabel('Neuron Index', fontsize=16)
+pl.title('Simple E-I network', fontsize=16)
+pl.tick_params(labelsize=16)
+pl.legend(loc='best')
 pl.show()
