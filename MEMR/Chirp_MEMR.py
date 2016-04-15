63b925920ecae53ec77ff952aeedc65c1e8b1cfc
Hari Bharadwaj <hari@nmr.mgh.harvard.edu>
2016-04-15 11:49:57 -0400

    Small edits.. Cross-frequency coupling model parameter regime tested

--- MEMR/Chirp_MEMR.py
+++ MEMR/Chirp_MEMR.py
@@ -20,60 +20,61 @@ for subj in subjlist:
     # These are so that the generated files are organized better
     respath = fpath
 
-    cond = [1, 2]
-    condstem = 'chirp'
-    print 'Running Subject', subj, 'Condition', cond
+    conds = [1, 2]
+    condstems = ['chirp_cond', 'chirp_rare']
+    for k, cond in enumerate(conds):
+        condstem = condstems[k]
+        print 'Running Subject', subj, 'Condition', cond
+        save_raw_name = subj + '_' + condstem + '_alltrial.mat'
 
-    save_raw_name = subj + '_' + condstem + '_alltrial.mat'
+        if os.path.isfile(respath + save_raw_name) and not overwriteOld:
+            print 'Epoched data is already available on disk!'
+            print 'Loading data from:', respath + save_raw_name
+            x = io.loadmat(respath + save_raw_name)['x']
+            fs = 4096.0
+        else:
+            bdfs = fnmatch.filter(os.listdir(fpath), subj +
+                                  '_' + '*Chirp*.bdf')
 
-    if os.path.isfile(respath + save_raw_name) and not overwriteOld:
-        print 'Epoched data is already available on disk!'
-        print 'Loading data from:', respath + save_raw_name
-        x = io.loadmat(respath + save_raw_name)['x']
-        fs = 4096.0
-    else:
-        bdfs = fnmatch.filter(os.listdir(fpath), subj +
-                              '_' + '*Chirp*.bdf')
+            print 'No pre-epoched data found, looking for BDF files'
+            print 'Viola!', len(bdfs),  'files found!'
 
-        print 'No pre-epoched data found, looking for BDF files'
-        print 'Viola!', len(bdfs),  'files found!'
+            for k, edfname in enumerate(bdfs):
+                # Load data and read event channel
+                (raw, eves) = bs.importbdf(fpath + edfname, nchans=35,
+                                           refchans=['EXG1', 'EXG2'])
 
-        for k, edfname in enumerate(bdfs):
-            # Load data and read event channel
-            (raw, eves) = bs.importbdf(fpath + edfname, nchans=35,
-                                       refchans=['EXG1', 'EXG2'])
-            '''
-            raw.info['bads'] += ['EXG3', 'A1', 'A2', 'A30', 'A7', 'A6',
-                                 'A24', 'A28', 'A29', 'A3', 'A11', 'A15',
-                                 'A16', 'A17', 'A10', 'A21', 'A20', 'A25']
-            raw.drop_channels(raw.info['bads'])
-            '''
+                raw.info['bads'] += ['EXG3', 'A1', 'A2', 'A30', 'A7', 'A6',
+                                     'A24', 'A28', 'A29', 'A3', 'A11', 'A15',
+                                     'A16', 'A17', 'A10', 'A21', 'A20', 'A25']
+                raw.drop_channels(raw.info['bads'])
 
-            # Filter the data
-            raw.filter(
-                l_freq=15, h_freq=1000, picks=np.arange(30, 32, 1))
+                # Filter the data
+                raw.filter(
+                    l_freq=15, h_freq=1000, picks=np.arange(0, 17, 1))
 
-            # raw.apply_proj()
-            fs = raw.info['sfreq']
+                # raw.apply_proj()
+                fs = raw.info['sfreq']
 
-            # Epoching events of type
-            epochs = mne.Epochs(
-                raw, eves, cond, tmin=-0.2, proj=False,
-                tmax=8.2, baseline=(-0.1, 0.0), picks=np.arange(30, 32, 1),
-                reject=dict(eeg=200e-6))
+                # Epoching events of type
+                epochs = mne.Epochs(
+                    raw, eves, cond, tmin=-0.5, proj=False,
+                    tmax=8.2, baseline=(-0.1, 0.0), picks=np.arange(0, 17, 1),
+                    reject=dict(eeg=200e-6))
 
-            xtemp = epochs.get_data()
+                xtemp = epochs.get_data()
 
-            # Reshaping to the format needed by spectral.mtcpca() and
-            # calling it
-            if(xtemp.shape[0] > 0):
-                xtemp = xtemp.transpose((1, 0, 2))
-                if(k == 0):
-                    x = xtemp
+                # Reshaping to the format needed by spectral.mtcpca() and
+                # calling it
+                if(xtemp.shape[0] > 0):
+                    xtemp = xtemp.transpose((1, 0, 2))
+                    xtemp = xtemp[:15, :, :]
+                    if(k == 0):
+                        x = xtemp
+                    else:
+                        x = np.concatenate((x, xtemp), axis=1)
                 else:
-                    x = np.concatenate((x, xtemp), axis=1)
-            else:
-                continue
+                    continue
 
-    if not os.path.isfile(respath + save_raw_name):
-        io.savemat(respath + save_raw_name, dict(x=x, subj=subj))
+        if not os.path.isfile(respath + save_raw_name):
+            io.savemat(respath + save_raw_name, dict(x=x, subj=subj))
--- Rhythms/PyNNGamma.py
+++ Rhythms/PyNNGamma.py
@@ -25,24 +25,24 @@ Esyn = sim.StaticSynapse(weight=0.05, delay=1.0)
 random = sim.FixedProbabilityConnector(p_connect=0.3)
 Econnections = sim.Projection(Ecells, Net, random, Esyn,
                               receptor_type='excitatory')
-Isyn = sim.StaticSynapse(weight=0.10, delay=1.0)
+Isyn = sim.StaticSynapse(weight=0.1, delay=1.0)
 Iconnections = sim.Projection(Icells, Net, random, Isyn,
                               receptor_type='inhibitory')
 
 noise1 = sim.NoisyCurrentSource(mean=0.6, stdev=0.3, start=50.0,
                                 stop=850.0, dt=0.01)
 
-Ecells.inject(noise1)
-# noise2 = sim.NoisyCurrentSource(mean=0.3, stdev=0.3, start=50.0,
-#                                 stop=850.0, dt=0.01)
+# Ecells.inject(noise1)
+noise2 = sim.NoisyCurrentSource(mean=0.3, stdev=0.3, start=50.0,
+                                stop=850.0, dt=0.01)
 
-# noise1.inject_into(Ecells[(0, 5, 10, 11)])
-# noise2.inject_into(Ecells[(3, 7, 15, 17)])
+noise1.inject_into(Ecells[(0, 5, 10, 11)])
+noise2.inject_into(Ecells[(3, 7, 15, 17)])
 
-# sine = sim.ACSource(start=50.0, stop=850.0, amplitude=1.0, offset=1.0,
-#                     frequency=25.0, phase=180.0)
+sine = sim.ACSource(start=50.0, stop=850.0, amplitude=2.0, offset=1.0,
+                    frequency=4.0, phase=180.0)
 
-# sine.inject_into(Ecells[(2, 25, 39)])
+sine.inject_into(Ecells[(2, 25, 39)])
 
 Net.record(['v', 'spikes'])
 sim.run(1000.0)
@@ -96,7 +96,7 @@ for k in range(nE + nI):
 
 pl.xlabel('Time (ms)', fontsize=16)
 pl.ylabel('Neuron Index', fontsize=16)
-pl.title('Simple E-I network', fontsize=16)
+pl.title('Network Properties', fontsize=16)
 pl.tick_params(labelsize=16)
 pl.legend(loc='best')
 pl.show()
